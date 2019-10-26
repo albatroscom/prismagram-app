061668ef5a208f6707ff2e46d8a24805bf9abc4e
albatroscom <albatroscom@naver.com>
2019-10-26 17:37:05 +0900

    #10.2 Preloading Cache

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
 import React, { useState, useEffect } from 'react';
 import { Ionicons } from '@expo/vector-icons';
 import { AppLoading } from 'expo';
-import { Text, View } from 'react-native';
+import { Text, View, AsyncStorage } from 'react-native';
 import * as Font from 'expo-font';
 import { Asset } from 'expo-asset';
+// import AsyncStorage from '@react-native-community/async-storage';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { persistCache } from 'apollo-cache-persist';
+import ApolloClient from 'apollo-boost';
+import apolloClientOptions from './apollo'; 
+import { ApolloProvider } from 'react-apollo-hooks';
 
 export default function App() {
   const [loaded, setLoaded] = useState(false);
+  const [client, setClient] = useState(null);
 
   // preload
   const preLoad = async () => {
@@ -14,8 +21,25 @@ export default function App() {
       await Font.loadAsync({
         ...Ionicons.font
       });
-      await Asset.loadAsync([require("./assets/app_image.png")]);
+      await Asset.loadAsync([require("./assets/logo.png")]);
+
+      const cache = new InMemoryCache();
+
+      // await before instantiating ApolloClient, else queries might run before the cache is persisted
+      await persistCache({
+        cache,
+        storage: AsyncStorage,
+      });
+      
+      // Continue setting up Apollo as usual.
+      const client = new ApolloClient({
+        cache,
+        ...apolloClientOptions
+      });
+
       setLoaded(true);
+      setClient(client);
+
     } catch (e) {
       console.log(e);
     }
@@ -24,11 +48,14 @@ export default function App() {
     preLoad();
   }, []);
 
-  return loaded ? (
-    <View style={{ top: '10%', left: '10%'}}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text>It works!</Text>
-    </View>
+  // check loaded and client called
+  return loaded && client ? (
+    <ApolloProvider client={client}>
+      <View style={{ top: '10%', left: '10%'}}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <Text>It works!</Text>
+      </View>
+    </ApolloProvider>
   ) : (
     <AppLoading />
   );
