7824bd2d09b1c4a187d269e545025d8242fa4a3b
albatroscom <albatroscom@naver.com>
2019-11-03 18:52:07 +0900

    10.5 isLoggedIn part One

--- App.js
+++ App.js
@@ -2,18 +2,24 @@ import React, { useState, useEffect } from 'react';
 import { Ionicons } from '@expo/vector-icons';
 import { AppLoading } from 'expo';
 import { Text, View, AsyncStorage } from 'react-native';
-import * as Font from 'expo-font';
-import { Asset } from 'expo-asset';
+import * as Font from 'expo-font'; // expo 에서 변경된 내용
+import { Asset } from 'expo-asset'; // expo 에서 변경된 내용
 // import AsyncStorage from '@react-native-community/async-storage';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { persistCache } from 'apollo-cache-persist';
 import ApolloClient from 'apollo-boost';
+import { ThemeProvider } from 'styled-components';
 import apolloClientOptions from './apollo'; 
 import { ApolloProvider } from 'react-apollo-hooks';
+import styles from './styles';
 
 export default function App() {
   const [loaded, setLoaded] = useState(false);
   const [client, setClient] = useState(null);
+
+  // localStorage 에 접근해서 사용자 로그인 여부를 확인해서, 로그인 여부에 따라 다른 router(navigator) 를 리턴하기 위함 
+  // 로그인 여부를 체크한후에 로그아웃을 했는지 알아내기 위해서 null 로 기본값 설정 : null - 체크 x, false - 체크 O (로그아웃함), true - 체크 O (로그인함)
+  const [isLoggedIn, setIsLoggedIn] = useState(null);
 
   // preload
   const preLoad = async () => {
@@ -37,6 +43,13 @@ export default function App() {
         ...apolloClientOptions
       });
 
+      const isLoggedIn = await AsyncStorage.getItem('isLoggedIn');
+      if (isLoggedIn === null || isLoggedIn === false) {
+        setIsLoggedIn(false);
+      } else {
+        setIsLoggedIn(true);
+      }
+
       setLoaded(true);
       setClient(client);
 
@@ -49,12 +62,13 @@ export default function App() {
   }, []);
 
   // check loaded and client called
-  return loaded && client ? (
+  return loaded && client && isLoggedIn !== null ? (
     <ApolloProvider client={client}>
-      <View style={{ top: '10%', left: '10%'}}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>It works!</Text>
-      </View>
+      <ThemeProvider theme={styles}>
+        <View style={{ top: '10%', left: '10%'}}>
+        { isLoggedIn === true ? <Text>I'm In</Text> : <Text>I'm Out</Text> }
+        </View>
+      </ThemeProvider>
     </ApolloProvider>
   ) : (
     <AppLoading />
